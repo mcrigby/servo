3861dc452b34cfd4231b23c27301ee0b29d89f5c
Martin Cutillo-Rigby <70958005+mcrigby@users.noreply.github.com>
2023-03-24 14:56:04 +0000

    add new customservomap method

--- CutilloRigby.Output.Servo/ServoMap.cs
+++ CutilloRigby.Output.Servo/ServoMap.cs
@@ -19,8 +19,8 @@ public sealed class ServoMap : IServoMap
         }
     }
 
-    public static IServoMap LinearServoMap() => CustomServoMap(0, x => (float)((x / 5120) + 0.05));
-    public static IServoMap SignedServoMap() => CustomServoMap(-128, x => (float)(((x + 128) / 5120) + 0.05), x => x < 0 ? 256 + x : x);
+    public static IServoMap LinearServoMap() => CustomServoMap();
+    public static IServoMap SignedServoMap() => CustomServoMap(-128);
     public static IServoMap CustomServoMap(int rangeStart, Func<float, float> dutyCycleCalculation, Func<int, int>? outputOrder = null)
     {
 
@@ -28,6 +28,19 @@ public sealed class ServoMap : IServoMap
             .OrderBy(x => outputOrder?.Invoke(x) ?? x)
             .Select(x => (float)x)
             .Select(dutyCycleCalculation)
+            .ToArray();
+
+        return new ServoMap(values);
+    }
+    public static IServoMap CustomServoMap(short rangeStart = 0, short rangeLength = 256, float dutyCycleMin = 0.05f, float dutyCycleMax = 0.10f)
+    {
+        var rangeOffset = 0 - rangeStart;
+        var dutyCycleRange = dutyCycleMax - dutyCycleMin;
+        var stepFactor = (rangeLength - 1) / dutyCycleRange;
+
+        var values = Enumerable.Range(rangeStart, rangeLength)
+            .OrderBy(x => x < 0 ? rangeLength + x : x)
+            .Select(x => ((x + rangeOffset) / stepFactor) + dutyCycleMin)
             .ToArray();
 
         return new ServoMap(values);
